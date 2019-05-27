660f226fe09b6848b9599e17c53fbcb8a82ce3d1
Ali HADJ CHAIB <redhat78@gmail.com>
2019-05-27 17:14:19 +0200

    Creation subnet

--- host.tf
+++ host.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "mmh" {
-  ami = "ami-5026902d"
+  ami           = "ami-0f3eb76b0366d975f"
   instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.mmh_subnet.id}"
   tags {
     Name = "server_mmh"
   }
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,6 @@
 #create vpc
 resource "aws_vpc" "mmh" {
-  cidr_block       = "172.23.0.0/16"
-  instance_tenancy = "dedicated"
+  cidr_block = "172.23.0.0/16"
 
   tags = {
     Name = "vpc MMH"
